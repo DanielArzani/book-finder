4ba6a99de9163a561e41e99d0a77e805311fa19d
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2022-03-15 16:09:54 -0400

    attempt to fix error, create a commit at this point so I can always come back to here

--- server/server.js
+++ server/server.js
@@ -36,11 +36,10 @@ if (process.env.NODE_ENV === "production") {
 //TODO: DELETE THIS
 app.use(routes);
 
-//TODO: TEST THIS IN PRODUCTION
 // send index.html for any routes that don't exist
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// })
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));

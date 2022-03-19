c7ef56dc5983e786c1bac85ff6143d1fcb57bf86
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2022-03-19 10:51:28 -0400

    fix error

--- server/server.js
+++ server/server.js
@@ -4,8 +4,6 @@ const { authMiddleware } = require("./utils/auth");
 
 const path = require("path");
 const db = require("./config/connection");
-//TODO: DELETE THIS
-const routes = require("./routes");
 
 const { typeDefs, resolvers } = require("./schema/");
 
@@ -22,7 +20,6 @@ const startServer = async () => {
   server.applyMiddleware({ app });
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 };
-
 startServer();
 
 app.use(express.urlencoded({ extended: true }));
@@ -31,15 +28,12 @@ app.use(express.json());
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
+
+  // send index.html for any routes that don't exist
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
 }
-
-// send index.html for any routes that don't exist
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
-
-//TODO: DELETE THIS
-app.use(routes);
 
 db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
